2bd5e487f1115806662dd376dde2c1064c5353b4
Ondra58 <97514734+Ondra58@users.noreply.github.com>
2023-04-02 22:13:02 +0200

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(107, 178);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(311, 178);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileStream soubor = new FileStream("texty.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter zapisovac = new BinaryWriter(soubor, Encoding.GetEncoding("windows-1250"));
+            BinaryReader ctenar = new BinaryReader(soubor, Encoding.GetEncoding("windows-1250"));
+            for(int i =1; i <= 10;i++)
+            {
+                zapisovac.Write("This is věta číslo " + i + ".");
+            }
+            ctenar.BaseStream.Position = 0;
+            while (ctenar.BaseStream.Position < ctenar.BaseStream.Length)
+            {
+                listBox1.Items.Add(ctenar.ReadString());
+            }
+            soubor.Close();
+            soubor = new FileStream("texty.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            zapisovac = new BinaryWriter(soubor, Encoding.GetEncoding("windows-1250"));
+            foreach (string str in listBox1.Items)
+            {
+                zapisovac.Write(str.Replace('.', '!'));
+            }
+            ctenar = new BinaryReader(soubor, Encoding.GetEncoding("windows-1250"));
+            ctenar.BaseStream.Position = 0;
+            while (ctenar.BaseStream.Position < ctenar.BaseStream.Length)
+            {
+                listBox2.Items.Add(ctenar.ReadString());
+            }
+            soubor.Close();
+        }
+
+    }
+}
